06d611adaf0d3d8aee19b574d9f2efb26f54e339
SuJeong0212 <s.rystal0212@gmail.com>
2022-01-04 17:04:07 +0900

    💄 SetInterval & ClearInterval = timer

--- assets/js/Timer.js
+++ assets/js/Timer.js
@@ -2,6 +2,8 @@ window.onload = function(){
     var num = document.querySelector(".num");
     var button = document.querySelector(".startBtn");
     var okBtn = document.querySelector(".okBtn");
+    var input = document.getElementsByTagName("input")[0];
+    var sec = document.querySelector(".sec");
 
     button.addEventListener("click", function(){
         interval();
@@ -16,6 +18,27 @@ window.onload = function(){
         timer = setInterval(function(){
             secNum ++;
             num.innerHTML = secNum;
-        },1000);
+        },100);
     }
+
+    okBtn.addEventListener("click", function(){
+        interval_timer();
+    });
+
+    // var timer1;
+
+    function interval_timer(){
+        clearInterval(timer);
+
+        value = input.value * 10000;
+        secValue = value / 365 / 24 / 60 / 60;
+
+        secNum = 0;
+
+        timer = setInterval(function(){
+            secNum ++;
+            sec.innerHTML = "당신은 " + secNum + "초에 " + (secValue * secNum).toFixed(1) + "원 벌고 있습니다.";
+        },1000)
+    }
+
 }
