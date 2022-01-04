f4c914d1ffc4e9d7a1c3178df6011fe5fc7241e1
SuJeong0212 <s.rystal0212@gmail.com>
2022-01-04 19:55:14 +0900

    💄 Paging -> Nike

---
+++ assets/js/paging2.js
@@ -0,0 +1,58 @@
+window.onload = function(){
+    var section = document.getElementsByTagName("section");
+    // var pointBtn = document.querySelector('.pointWrap').querySelectorAll("li");
+    var pointBtn = document.querySelectorAll('.pointWrap li');
+
+    var pageNum = 0;
+    var totalNum = section.length;
+    // alert(pointBtn.length);
+
+
+
+    for(var i=0; i < pointBtn.length; i++){
+        (function(idx){
+            pointBtn[idx].onclick = function() {
+                pageNum = idx;
+
+                pageChangeFunc();
+
+                window.scrollTo({
+                    top: section[pageNum].offsetTop,
+                    behavior:'smooth',
+                })
+            }
+        })(i);
+    }
+
+
+    //스크롤 이벤트
+    window.addEventListener("scroll", function(event){
+      var scroll = this.scrollY;
+
+      for(var i=0; i<totalNum; i++){
+        if(scroll > section[i].offsetTop - window.outerHeight/1.5  && scroll < section[i].offsetTop - window.outerHeight/1.5 + section[i].offsetHeight){
+          pageNum = i;
+          break;
+        }
+      }
+      pageChangeFunc();
+    });
+
+
+    //페이지 변경
+    function pageChangeFunc(){
+      for(var i=0; i<totalNum; i++){
+        section[i].classList.remove("active");
+        // section[i].className = "";
+        pointBtn[i].classList.remove("active");
+      }
+      section[pageNum].classList.add("active");
+      pointBtn[pageNum].classList.add("active");
+      
+    }
+
+
+
+    //페이지 로드되면 바로 실행
+    pageChangeFunc();
+  }
